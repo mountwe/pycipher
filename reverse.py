01f9408e230af642885ebb7a02c3c9b3fa72ea4a
Gene Mountjoy <mountwe@live.com>
2019-09-08 16:10:53 -0400

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,12 @@
+# Reverse Cipher
+# https://www.nostarch.com/crackingcodes/ (BSD License)
+
+input = "Type in a message to be printed out reversed: "
+translated = ''
+
+i = len(message) - 1
+while i >= 0:
+    translated = translated + message[i]
+    i = i - 1
+
+print(translated)
